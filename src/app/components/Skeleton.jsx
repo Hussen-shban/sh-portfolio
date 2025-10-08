d140ac0289bf22dd41000e84845f94b4fefe9e52
hussen <hussenshban200041@gmail.com>
2025-10-08 11:35:48 +0100

    new

--- src/app/components/Skeleton.jsx
+++ src/app/components/Skeleton.jsx
@@ -2,15 +2,30 @@
 
 import { DotLottiePlayer } from '@dotlottie/react-player'
 import '@dotlottie/react-player/dist/index.css'
+import { motion } from 'framer-motion'
 const Skeleton = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            <DotLottiePlayer
-                src="/lotties/death-dance.json"  // مسار الملف داخل public
-                autoplay
-                loop
-                style={{ width: 100, height: 100 }}
-            />
+
+            <motion.div
+            
+            drag // ⬅️ هذا يسمح بالسحب أفقياً (يمين/يسار)
+            dragConstraints={{ left: -200, right: 200 }} // حدود الحركة
+            dragElastic={0.2} // مرونة السحب (0 = ثابت، 1 = مطاطي)
+            whileTap={{ scale: 1.05 }} // تأثير بسيط لما تمسك العنصر
+            className="cursor-grab active:cursor-grabbing"
+            >
+
+
+                <DotLottiePlayer
+                    src="/lotties/death-dance.json"  // مسار الملف داخل public
+                    autoplay
+                    loop
+                    style={{ width: 100, height: 100 }}
+
+                />
+
+            </motion.div>
         </div>
     )
 }
